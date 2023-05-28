2138330a8d5e399ee16de38ff864da16026e22db
Brayan Noe Aguilar Jarquin <Brayan2noe@gmail.com>
2023-05-28 13:13:07 -0600

    Agregacion de interfaz

---
+++ src/InterfazG/Interfaz.java
@@ -0,0 +1,11 @@
+package InterfazG;
+import javax.swing.JFrame;
+import java.awt.*;
+
+
+public class Interfaz extends JFrame {
+
+    public Interfaz(){
+        this.setSize(500,500);
+    }
+}
---
+++ src/Test/Main.java
@@ -0,0 +1,10 @@
+package Test;
+
+import InterfazG.Interfaz;
+
+public class Main {
+    public static void main(String[] args) {
+        Interfaz i1= new Interfaz();
+        i1.setVisible(true);
+    }
+}
